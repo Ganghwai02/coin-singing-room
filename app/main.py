71753af3997e7c0db47b449f8f700760a21a085b
갱화이 <dkdldb9305@gmail.com>
2026-01-15 18:36:34 +0900

    CORS 허용

--- app/main.py
+++ app/main.py
@@ -49,7 +49,7 @@ app.openapi = custom_openapi
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
